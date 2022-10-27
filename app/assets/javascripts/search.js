9296ca6d8fd7a979940d40b3b9ec30915d27d0ad
Sulagna Giri <girisulagna2000@gmail.com>
2022-10-27 18:17:46 +0530

    CSS

--- app/assets/javascripts/search.js
+++ app/assets/javascripts/search.js
@@ -1,25 +1,50 @@
 function searchStore(){
-    // Declare variables 
-    var input, filter, table, tr, td, i;
-    input = document.getElementById("searchbar");
-    filter = input.value.toLowerCase();
-    table = document.getElementById("table_id");
-    tr = table.getElementsByTagName("tr");
+  // Declare variables 
+  var input, filter, table, tr, td, i;
+  input = document.getElementById("searchbar");
+  filter = input.value.toLowerCase();
+  table = document.getElementById("table_id");
+  tr = table.getElementsByTagName("tr");
 
-    // Loop through all table rows, and hide those who don't match the search query
-    for (i = 0; i < tr.length; i++) {
-      td = tr[i].getElementsByTagName("td") ; 
-      for(j=0 ; j<td.length ; j++)
-      {
-        let tdata = td[j] ;
-        if (tdata) {
-          if (tdata.innerHTML.toLowerCase().indexOf(filter) > -1) {
-            tr[i].style.display = "";
-            break ; 
-          } else {
-            tr[i].style.display = "none";
-          }
-        } 
-      }
+  // Loop through all table rows, and hide those who don't match the search query
+  for (i = 0; i < tr.length; i++) {
+    td = tr[i].getElementsByTagName("td") ; 
+    for(j=0 ; j<td.length ; j++)
+    {
+      let tdata = td[j] ;
+      if (tdata) {
+        if (tdata.innerHTML.toLowerCase().indexOf(filter) > -1) {
+          tr[i].style.display = "";
+          break ; 
+        } else {
+          tr[i].style.display = "none";
+        }
+      } 
     }
   }
+}
+
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
